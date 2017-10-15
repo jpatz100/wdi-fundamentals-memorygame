7cfea0e765156e6d58361583c74a63e46df28e76
jpatz100 <jpatruno1924@gmail.com>
2017-10-14 22:23:42 -0400

    local repository

---
+++ js/main.js
@@ -0,0 +1,33 @@
+console.log("Up and running!")
+var cards = [
+{
+card: 'queen',
+},
+{
+card: 'king'
+}
+];
+
+
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+var flipcard = function() {
+	this.getAttribute('data-id');
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+flipcard[0];
+
+var createBoard = function() { 
+for (var i = 0; i < arrayName.length; i++) {
+}
+};
+
+var newListItem = document.createElement('li');
+cardElement.setAttribute(i);
+
+createBoard();
